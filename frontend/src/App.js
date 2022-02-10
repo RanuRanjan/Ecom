6ae17ff41852aa5f19e3fa6767045fc818b9c1c0
RanuRanjan <ranuranjan25@gmail.com>
2022-02-10 13:45:36 +0530

    frontend setup completed

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import {Container} from 'react-bootstrap'
+function App() {
+  return (
+    < >
+      <Container>
+
+        <h1>Ecomm App</h1>
+      </Container>
+      
+    </>
+  );
+}
+
+export default App;
